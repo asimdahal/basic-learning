b6ae292196c2612d37c43047f0f9b4c1583f51a1
asimdahal <asim.dahal2001@gmail.com>
2019-11-16 10:56:15 -0500

    Update Age.java
    now the calculator won't calculate future dates for birthday

--- Age.java
+++ Age.java
@@ -17,33 +17,30 @@ public class Age {
         int diffY = cuYear - year;
         int diffM;
         int diffD;
-        if (cuMonth < month)
-        {
-            diffY--;
-            diffM = month;
+
+        if(year>cuYear){
+            System.out.println("You cannot be born in the future");
         }
-        else if((cuMonth == month) && (cuDay < day))
-        {
-            diffY--;
-            diffM = month;
+        else {
+
+            if (cuMonth < month) {
+                diffY--;
+                diffM = month;
+            } else if ((cuMonth == month) && (cuDay < day)) {
+                diffY--;
+                diffM = month;
+            } else {
+                diffM = cuMonth - month;
+            }
+            if ((month == 11 || month == 4 || month == 6 || month == 9) && cuDay < day) {
+                diffD = 30 - (day - cuDay);
+            } else if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && cuDay < day) {
+                diffD = 31 - (day - cuDay);
+            } else {
+                diffD = cuDay - day;
+            }
+            System.out.println("Age = " + " Year: " + diffY + " Month: " + diffM + " Day: " + diffD);
         }
-        else
-        {
-            diffM = cuMonth - month;
-        }
-        if((month == 11 || month == 4 || month == 6 || month == 9) && cuDay < day)
-        {
-            diffD = 30-(day-cuDay);
-        }
-        else if((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && cuDay < day)
-        {
-            diffD = 31-(day-cuDay);
-        }
-        else
-        {
-            diffD = cuDay - day;
-        }
-        System.out.println("Age = " + " Year: " + diffY +  " Month: " + diffM + " Day: "+ diffD );
 
     }
 }
